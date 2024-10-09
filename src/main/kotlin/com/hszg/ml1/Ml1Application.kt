e9bd150bfef7ffc2472d4bd19195741a5d576a7d
JonathanZopf <jonathanzopf@gmail.com>
2024-10-09 11:16:16 +0200

    Initial commit

---
+++ src/main/kotlin/com/hszg/ml1/Ml1Application.kt
@@ -0,0 +1,11 @@
+package com.hszg.ml1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ml1Application
+
+fun main(args: Array<String>) {
+	runApplication<Ml1Application>(*args)
+}
---
+++ src/test/kotlin/com/hszg/ml1/Ml1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hszg.ml1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ml1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
